db0d34ec3bff9498918e9808486d1c236de8eed1
JL-Technobel-edu <111964225+JL-Technobel-edu@users.noreply.github.com>
2023-08-05 02:40:39 +0200

    2

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,14 +8,14 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = (new ConfigService()).get('PORT');
+  const PORT = new ConfigService().get('PORT');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '../src/uploads'));
-  app.setGlobalPrefix('v1/api');  
+  app.setGlobalPrefix('v1/api');
   //app.use(cookieParser());
   app.enableCors();
   app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true,transform:true  }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
     .setTitle('Atlas Project')
